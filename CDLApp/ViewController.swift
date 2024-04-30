e10956771dedf4960979eab9f8c2d29c6f203771
bekturinho <ismanalievbektur4@gmail.com>
2024-04-30 17:02:23 -0500

    Initial Commit

---
+++ CDLApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CDLApp
+//
+//  Created by fortune cookie on 4/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
